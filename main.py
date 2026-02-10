cd290a3101b06971d8bf83f28fbe630da386660c
lllcrazydoglll-cpu <lllcrazydoglll@gmail.com>
2026-02-10 19:08:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -64,10 +64,13 @@ if st.button("提交日報表"):
         st.error(f"❌ 發生錯誤：{e}")
 
 def get_gspread_client():
+    # 讀取 Secrets
     info = dict(st.secrets["gcp_service_account"])
-    # 強制將可能誤入的 "\\n" 轉回真正的換行符號
-    info["private_key"] = info["private_key"].replace("\\n", "\n")
     
+    # 這是最後一道防線：如果裡面還有任何隱形的 \n 字串，強制轉換
+    if "\\n" in info["private_key"]:
+        info["private_key"] = info["private_key"].replace("\\n", "\n")
+        
     scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
     creds = Credentials.from_service_account_info(info, scopes=scope)
     return gspread.authorize(creds)
